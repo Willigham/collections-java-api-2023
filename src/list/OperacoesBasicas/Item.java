1f64fae84fb27a68fb0965071366fc26c7b22515
Willigham Coelho <willigamcoelho@gmail.com>
2025-03-28 01:04:24 -0300

    Commit 2

--- src/list/OperacoesBasicas/Item.java
+++ src/list/OperacoesBasicas/Item.java
@@ -1,4 +1,16 @@
 package list.OperacoesBasicas;
 
 public class Item {
+    //atributo
+    private String nome;
+
+    public Item(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
 }
